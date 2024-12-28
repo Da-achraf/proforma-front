77e23f9dd2ae80f90ace7615acebfde6b7852cab
Boukir <achraf.boukir@te.com>
2024-12-28 22:41:50 +0100

    f/enrich-reporting-table
    - fixed report rows color issue
    - enabled report generation in the
    warehouse approver view

--- src/app/shared/services/report-cell-color.service.ts
+++ src/app/shared/services/report-cell-color.service.ts
@@ -1,14 +1,14 @@
 import { Injectable } from '@angular/core';
 
 export const ReportCellColor = {
-  LightBlue: '#90d5ff', // Light Blue
-  LightCoral: '#FFCC99', // Light Coral
-  LightGreen: '#99FF99', // Light Green
-  LightYellow: '#FFFF99', // Light Yellow
-  LightPink: '#FF99CC', // Light Pink
-  LightPurple: '#CC99FF', // Light Purple
-  LightOrange: '#FFCC99', // Light Orange
-  LightTeal: '#99FFFF', // Light Teal
+  LightBlue: '#dddce0', // Light Blue
+  LightCoral: '#fbf8ff', // Light Coral
+  // LightGreen: '#99FF99', // Light Green
+  // LightYellow: '#FFFF99', // Light Yellow
+  // LightPink: '#FF99CC', // Light Pink
+  // LightPurple: '#CC99FF', // Light Purple
+  // LightOrange: '#FFCC99', // Light Orange
+  // LightTeal: '#99FFFF', // Light Teal
 };
 
 @Injectable({
